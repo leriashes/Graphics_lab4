9d8cfc2b9b2fabfbf3959dcc81e043d2049682d0
leriahes <valeriya-shevchenko-2002@mail.ru>
2023-11-15 15:37:54 +0700

    Отображение объектов

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ class App:
             eulers = [0, 0, 0]
         )
 
-        self.cube_mesh = CubeMesh()
+        #self.cube_mesh = CubeMesh()
+        self.cube_mesh = Mesh("models/glass.obj")
 
         self.wood_texture = Material("gfx/wood.png")
 
@@ -53,10 +54,6 @@ class App:
         self.mainLoop()
 
     def createShader(self, vertexFilepath, fragmentFilepath):
-        
-        #glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE)
-        #glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
-
         with open(vertexFilepath, 'r') as f:
             vertex_src = f.readlines()
 
@@ -77,7 +74,7 @@ class App:
                     running = False
 
             #обновление куба
-            self.cube.eulers[2] += 0.2
+            self.cube.eulers[2] += 0.25
             if self.cube.eulers[2] > 360:
                 self.cube.eulers[2] -= 360
 
@@ -119,166 +116,104 @@ class App:
         glDeleteProgram(self.shader)
         pg.quit()
 
-class CubeMesh:
-    def __init__(self):
-
-        #вершины - x, y, z, s, t
-        self.vertices = (
-            -0.5, -0.5, -0.5, 0, 0,
-            0.5, -0.5, -0.5, 1, 0,
-            0.5, 0.5, -0.5, 1, 1,
-
-            0.5, 0.5, -0.5, 1, 1,
-            -0.5, 0.5, -0.5, 0, 1,
-            -0.5, -0.5, -0.5, 0, 0,
-
-            -0.5, -0.5, 0.5, 0, 0,
-            0.5, -0.5, 0.5, 1, 0,
-            0.5, 0.5, 0.5, 1, 1,
-
-            0.5, 0.5, 0.5, 1, 1,
-            -0.5, 0.5, 0.5, 0, 1,
-            -0.5, -0.5, 0.5, 0, 0,
-
-            -0.5, 0.5, 0.5, 1, 0,
-            -0.5, 0.5, -0.5, 1, 1,
-            -0.5, -0.5, -0.5, 0, 1,
-
-            -0.5, -0.5, -0.5, 0, 1,
-            -0.5, -0.5, 0.5, 0, 0,
-            -0.5, 0.5, 0.5, 1, 0,
-
-            0.5, 0.5, 0.5, 1, 0,
-            0.5, 0.5, -0.5, 1, 1,
-            0.5, -0.5, -0.5, 0, 1,
-
-            0.5, -0.5, -0.5, 0, 1,
-            0.5, -0.5, 0.5, 0, 0,
-            0.5, 0.5, 0.5, 1, 0,
-
-            -0.5, -0.5, -0.5, 0, 1,
-            0.5, -0.5, -0.5, 1, 1,
-            0.5, -0.5, 0.5, 1, 0,
-
-            0.5, -0.5, 0.5, 1, 0,
-            -0.5, -0.5, 0.5, 0, 0,
-            -0.5, -0.5, -0.5, 0, 1,
-
-            -0.5, 0.5, -0.5, 0, 1,
-            0.5, 0.5, -0.5, 1, 1,
-            0.5, 0.5, 0.5, 1, 0,
-
-            0.5, 0.5, 0.5, 1, 0,
-            -0.5, 0.5, 0.5, 0, 0,
-            -0.5, 0.5, -0.5, 0, 1
-        )
-
-
-        self.vertex_count = len(self.vertices) // 5
-
-        self.vertices = np.array(self.vertices, dtype=np.float32)   #тип важен для правильного распознавания OpenGl
-
-        self.vao = glGenVertexArrays(1) #vertex array - массив вершин
-        glBindVertexArray(self.vao)
-        self.vbo = glGenBuffers(1)  #буфер вершин
-        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
-        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
-
-        #атрибут 0 - позиция
-        glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(0))
-        #0 - номер атрибута
-        #3 - количество значений
-        #тип значения
-        #нужна ли нормализация значений
-        #количество байт для перехода к след. значению
-        #смещение
-
-        #атрибут 1 - позиция текстуры
-        glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(12))
-
-
-    #очистка памяти
-    def destroy(self):
-        glDeleteVertexArrays(1, (self.vao,))
-        glDeleteBuffers(1, (self.vbo,))
-
 class Cube:
     def __init__(self, position, eulers):
         self.position = np.array(position, dtype=np.float32)
         self.eulers = np.array(eulers, dtype=np.float32)
-'''
-        glUseProgram(shader)
 
-        #вершины - x, y, z, s, t, nx, ny, nz
-        self.vertices = (
-            -0.5, -0.5, -0.5, 0, 0, 0, 0, -1,
-            0.5, -0.5, -0.5, 1, 0, 0, 0, -1,
-            0.5, 0.5, -0.5, 1, 1, 0, 0, -1,
-
-            0.5, 0.5, -0.5, 1, 1, 0, 0, -1,
-            -0.5, 0.5, -0.5, 0, 1, 0, 0, -1,
-            -0.5, -0.5, -0.5, 0, 0, 0, 0, -1,
-
-            -0.5, -0.5, 0.5, 0, 0, 0, 0, 1,
-            0.5, -0.5, 0.5, 1, 0, 0, 0, 1,
-            0.5, 0.5, 0.5, 1, 1, 0, 0, 1,
-
-            0.5, 0.5, 0.5, 1, 1, 0, 0, 1,
-            -0.5, 0.5, 0.5, 0, 1, 0, 0, 1,
-            -0.5, -0.5, 0.5, 0, 0, 0, 0, 1,
-
-            -0.5, 0.5, 0.5, 1, 0, -1, 0, 0,
-            -0.5, 0.5, -0.5, 1, 1, -1, 0, 0,
-            -0.5, -0.5, -0.5, 0, 1, -1, 0, 0,
-
-            -0.5, -0.5, -0.5, 0, 1, -1, 0, 0,
-            -0.5, -0.5, 0.5, 0, 0, -1, 0, 0,
-            -0.5, 0.5, 0.5, 1, 0, -1, 0, 0,
-
-            0.5, 0.5, 0.5, 1, 0, 1, 0, 0,
-            0.5, 0.5, -0.5, 1, 1, 1, 0, 0,
-            0.5, -0.5, -0.5, 0, 1, 1, 0, 0,
-
-            0.5, -0.5, -0.5, 0, 1, 1, 0, 0,
-            0.5, -0.5, 0.5, 0, 0, 1, 0, 0,
-            0.5, 0.5, 0.5, 1, 0, 1, 0, 0,
-
-            -0.5, -0.5, -0.5, 0, 1, 0, -1, 0,
-            0.5, -0.5, -0.5, 1, 1, 0, -1, 0,
-            0.5, -0.5, 0.5, 1, 0, 0, -1, 0,
-
-            0.5, -0.5, 0.5, 1, 0, 0, -1, 0,
-            -0.5, -0.5, 0.5, 0, 0, 0, -1, 0,
-            -0.5, -0.5, -0.5, 0, 1, 0, -1, 0,
-
-            -0.5, 0.5, -0.5, 0, 1, 0, 1, 0,
-            0.5, 0.5, -0.5, 1, 1, 0, 1, 0,
-            0.5, 0.5, 0.5, 1, 0, 0, 1, 0,
-
-            0.5, 0.5, 0.5, 1, 0, 0, 1, 0,
-            -0.5, 0.5, 0.5, 0, 0, 0, 1, 0,
-            -0.5, 0.5, -0.5, 0, 1, 0, 1, 0,
-        )
-
-        self.vertex_count = len(self.vertices) // 8
-        self.vertices = np.array(self.vertices, dtype=np.float32)
+class Mesh:
+    def __init__(self, filename):
+        #x, y, z, s, t, nx, ny, nz
+        vertices = self.loadMesh(filename)
+        self.vertex_count = len(vertices) // 8
+        vertices = np.array(vertices, dtype=np.float32)
 
         self.vao = glGenVertexArrays(1)
         glBindVertexArray(self.vao)
+
+        #вершины
         self.vbo = glGenBuffers(1)
         glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
-        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
+        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
 
-        glEnableVertexArrayAttrib(0)
+        #расположение
+        glEnableVertexAttribArray(0)
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
 
-        glEnableVertexArrayAttrib(1)
+        #текстура
+        glEnableVertexAttribArray(1)
         glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(12))
 
-        glEnableVertexArrayAttrib(2)
-        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(20))'''
+    def loadMesh(self, filename: str) -> list[float]:
+        v = []
+        vt = []
+        vn = []
+
+        vertices = []
+
+        with open(filename, 'r') as file:
+            line = file.readline()
+            while line:
+                words = line.split(" ")
+                if words[0] == "v":
+                    v.append(self.read_vertex_data(words))
+                elif words[0] == "vt":
+                    vt.append(self.read_texcoord_data(words))
+                elif words[0] == "vn":
+                    vn.append(self.read_normal_data(words))
+                elif words[0] == "f":
+                    self.read_face_data(words, v, vt, vn, vertices)
+                line = file.readline()
+
+        return vertices
+    
+    def read_vertex_data(self, words: list[str]) -> list[float]:
+        return [
+            float(words[1]),
+            float(words[2]),
+            float(words[3])
+        ]
+    
+    def read_texcoord_data(self, words: list[str]) -> list[float]:
+        return [
+            float(words[1]),
+            float(words[2])
+        ]
+
+    def read_normal_data(self, words: list[str]) -> list[float]:
+        return [
+            float(words[1]),
+            float(words[2]),
+            float(words[3])
+        ]
+
+    def read_face_data(self, words: list[str],
+                        v: list[list[float]], vt: list[list[float]], 
+                        vn: list[list[float]], vertices: list[float]) -> None:
+        triangleCount = len(words) - 3
+
+        for i in range(triangleCount):
+            self.make_corner(words[1], v, vt, vn, vertices)
+            self.make_corner(words[2 + i], v, vt, vn, vertices)
+            self.make_corner(words[3 + i], v, vt, vn, vertices)
+
+    def make_corner(self, corner_description: str,
+                    v: list[list[float]], vt: list[list[float]], 
+                    vn: list[list[float]], vertices: list[float]) -> None:
+        v_vt_vn = corner_description.split("/")
+        if (v_vt_vn[0] != '\n'):
+            for element in v[int(v_vt_vn[0]) - 1]:
+                vertices.append(element)
+
+            for element in vt[int(v_vt_vn[1]) - 1]:
+                vertices.append(element)
+
+            for element in vn[int(v_vt_vn[2]) - 1]:
+                vertices.append(element)
+
+    def destroy(self):
+        glDeleteVertexArrays(1, (self.vao,))
+        glDeleteBuffers(1, (self.vbo,))
 
 class Material:
     def __init__(self, filepath):
